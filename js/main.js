e75674304444898f65d1ea7a371dcb7ddec527e5
Lucas NAURY <38406425+lucasnaury@users.noreply.github.com>
2021-04-22 21:33:56 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-$(document).ready(function () { 
- 
+$(document).ready(function () {
+  $("#search").click(()=>{
+    console.log("Search");
+  });
+  $("#list").click(()=>{
+    window.location.href = window.location.href + "recipes.html";
+    console.log("Liste");
+  });
+  $("#add").click(()=>{
+    console.log("Add");
+  });
 });
