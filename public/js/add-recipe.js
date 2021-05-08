c230b31b3e03a8632e792cc2c594a634e40629a6
Lucas NAURY <38406425+lucasnaury@users.noreply.github.com>
2021-05-08 21:57:15 +0200

    Add files via upload

--- public/js/add-recipe.js
+++ public/js/add-recipe.js
@@ -1,3 +1,34 @@
 $(document).ready(function () {
+  var timePicker = new Picker(document.getElementById('time-input'),{
+    format: 'H:mm',
+    date: '1h00',
+    headers: true,
+    controls: false,
+    inline: true,
+    text: {
+      title: 'Choisissez une durée',
+      cancel: 'Annuler',
+      confirm: 'OK',
+      hour: 'Heure',
+      minute: 'Minute',
+    },
+    increment: {
+      hour: 1,
+      minute: 5
+    }
+  });
 
+
+
+
+  var picker2 = new Picker(document.getElementById('testing'),{
+    format: 'H m',
+    headers: true,
+    controls: false,
+    inline: true,
+    text: {
+      title: 'Pick a value',
+    },
+    values : ["test1","test2","test3"]
+  });
 });
